afeb588c18a5f12fb6eb6b8910f94c685919bfbd
dexterous17 <36617353+dexterous17@users.noreply.github.com>
2021-04-06 20:56:31 -0400

    InsertionSort

--- SortingAlogrithms/bubblesort.js
+++ SortingAlogrithms/bubblesort.js
@@ -1,7 +1,6 @@
 function bubbleSort(array) {
     for(let i=0;i<array.length;i++){
       for(let j=0;j<array.length-1-i;j++){
-          console.log(j)
         if(array[j]>array[j+1]){
           const temp = array[j];
           array[j] = array[j+1]
