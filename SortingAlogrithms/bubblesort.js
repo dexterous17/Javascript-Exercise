9971f16b484f15d63e4fee6e12aa0c18df2ae554
dexterous17 <36617353+dexterous17@users.noreply.github.com>
2021-04-06 20:58:58 -0400

    Bubblesort

--- SortingAlogrithms/bubblesort.js
+++ SortingAlogrithms/bubblesort.js
@@ -3,8 +3,8 @@ function bubbleSort(array) {
       for(let j=0;j<array.length-1-i;j++){
         if(array[j]>array[j+1]){
           const temp = array[j];
-          array[j] = array[j+1]
-          array[j+1] = temp
+          array[j] = array[j+1];
+          array[j+1] = temp;
         }
       }
     }
